dfd212e0b6fb5c96ae56af666507be982e7c46b2
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-08-10 17:19:29 -0400

    set-mongodb

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { log } = require("console");
+const MongoClient = require("mongodb").MongoClient;
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+let db;
 
+MongoClient.connect(
+  "mongodb+srv://cwpark1215:park1215@todoapp.amydrnc.mongodb.net/",
+  (error, client) => {
+    if (error) return console.log(error);
+
+    db = client.db("todoapp");
+
+    app.listen("8080", function () {
+      console.log("listening on 8080");
+    });
+  }
+);
 //REST 원칙
-
-app.listen(8080, () => {
-  console.log("listening on 8080");
-});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -20,5 +31,14 @@ app.get("/add", (req, res) => {
 
 app.post("/addTodo", (req, res) => {
   res.send("Complete!");
-  console.log(req.body.title);
+  db.collection("post").insertOne(
+    { title: req.body.title, date: req.body.date },
+    (err, result) => {
+      console.log("Complete");
+    }
+  );
+});
+
+app.get("/list", (req, res) => {
+  res.render("list.ejs");
 });
