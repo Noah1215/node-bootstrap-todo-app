8f07111d25c90c67d1685852de0eed6066d2cac4
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-08-10 20:07:54 -0400

    get-data-using-ejs

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 let db;
@@ -29,16 +28,37 @@ app.get("/add", (req, res) => {
   res.sendFile(__dirname + "/add.html");
 });
 
-app.post("/addTodo", (req, res) => {
-  res.send("Complete!");
-  db.collection("post").insertOne(
-    { title: req.body.title, date: req.body.date },
-    (err, result) => {
-      console.log("Complete");
-    }
-  );
+app.get("/list", (req, res) => {
+  db.collection("post")
+    .find()
+    .toArray((err, result) => {
+      if (err) {
+        return console.error(err);
+      }
+      res.render("list.ejs", {
+        posts: result,
+      });
+    });
 });
 
-app.get("/list", (req, res) => {
-  res.render("list.ejs");
+app.post("/addTodo", (req, res) => {
+  db.collection("counter").findOne({ name: "numOfPosts" }, (err, result) => {
+    let numOfPosts = result.totalposts;
+
+    db.collection("post").insertOne(
+      { _id: numOfPosts + 1, title: req.body.title, date: req.body.date },
+      (err, result) => {
+        console.log("Complete");
+        db.collection("counter").updateOne(
+          { name: "numOfPosts" },
+          { $inc: { totalposts: 1 } },
+          (err, result) => {
+            if (err) {
+              return console.error(err);
+            }
+          }
+        );
+      }
+    );
+  });
 });
